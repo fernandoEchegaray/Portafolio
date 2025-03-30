6bc41037844ab943628ee0b686000ce49341b957
Fernando Echegaray <148663054+fernandoEchegaray@users.noreply.github.com>
2025-03-30 12:25:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@ let sections = document.querySelector('section');
 let navLinks = document.querySelector('header nav a');
 
 window.onscroll = () => {
-  section.forEach(sec) => {
+  section.forEach(sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
-    let heigth = sec.offsetHeigth;
+    let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
     if(top >= offset && top < offset + heigth){
       navLinks.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
-      })
+        document.querySelector('header nav a[href*="' + id + '" ]').classList.add('active');
+      });
     }
-  })
-}
+  });
+};
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
