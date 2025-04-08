91047de9598a7e518da7a6591566509ddffaa442
Fernando Echegaray <148663054+fernandoEchegaray@users.noreply.github.com>
2025-04-08 19:13:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-
-    if(top >= offset && top < offset + height){
-      navLinks.forEach(link => {
-          link.classList.remove('active');
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.querySelector('#menu-icon');
+  const navbar = document.querySelector('.navbar');
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('header nav a');
+  window.onscroll = () => {
+    let timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      const top = window.scrollY;
+      sections.forEach(sec => {
+        const offset = sec.offsetTop - 150;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+          navLinks.forEach(link => {
+            link.classList.remove('active');
+          });
+          const activeLink = document.querySelector(`header nav a[href*="${id}"]`);
+          if (activeLink) {
+            activeLink.classList.add('active');
+          }
+          return; // Salir del bucle una vez que se encuentra la sección activa
+        }
       });
-      document.querySelectorAll('header nav a[href*="' + id + '"]').forEach(link => {
-        link.classList.add('active');
-      });
-      return;
-    }
-  });
-};
-
-menuIcon.onclick = () => { 
-  menuIcon.classList.toggle('bx-x'); 
-  navbar.classList.toggle('active');   
-}
-
+    }, 100); // Ajusta el tiempo según sea necesario
+  };
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  };
+});
